37901461f90845f35605b72330966b1d4a4a4cd1
jocelyn zheng <69088223+happired@users.noreply.github.com>
2026-01-10 13:25:24 -0500

    add module 0

--- content/guide/_meta.js
+++ content/guide/_meta.js
@@ -1,6 +1,7 @@
 export default {
   introduction: "Introduction",
   gettingstarted: "Getting Started",
+  module0: "Module 0",
   module1: "Module 1",
   module2: "Module 2",
   module3: "Module 3"
